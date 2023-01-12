0d1af8b6446494c6df678fc1f81e2b612aaa9b4c
Ashley Kapaso <ashleykapaso@gmail.com>
2023-01-12 19:07:46 +0200

    Added footer and reveal animation

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const addEventOnElements = function (elements, eventType, callback) {
-    for (let i = 0 , len = elements.length; i < len; i++) {
+    for (let i = 0, len = elements.length; i < len; i++) {
         elements[i].addEventListener(eventType, callback);
     }
 }
@@ -25,13 +25,17 @@ const navClose = function () {
 addEventOnElements(navLinks, "click", navClose);
 
 const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
 
 const activeElement = function () {
     if (window.scrollY > 100) {
         header.classList.add("active");
+        backTopBtn.classList.add("active");
+
     }
     else {
         header.classList.remove("active");
+        backTopBtn.classList.remove("active");
     }
 }
 
@@ -46,3 +50,19 @@ const buttonHoverRipple = function (event) {
 }
 
 addEventOnElements(buttons, "mousemove", buttonHoverRipple);
+
+const revealElements = document.querySelectorAll("[data-reveal]");
+
+const revealElementOnScroll = function () {
+  for (let i = 0, len = revealElements.length; i < len; i++) {
+    const isElementInsideWindow = revealElements[i].getBoundingClientRect().top < window.innerHeight / 1.1;
+
+    if (isElementInsideWindow) {
+      revealElements[i].classList.add("revealed");
+    }
+  }
+}
+
+window.addEventListener("scroll", revealElementOnScroll);
+
+window.addEventListener("load", revealElementOnScroll);
